8f3677a778d46fc214c8484c8c4e7f8caa02e21e
Jared Stearne <jared.stearne@gmail.com>
2021-11-29 10:46:17 -0800

    Ready to deploy, created a basic view.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views import View  # <- View class to handle requests
+# <- a class to handle sending a type of response
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
