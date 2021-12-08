346f2f15dc7d7dd9b3dc1ec1da187321d3cb445c
jstearne <jared.stearne@gmail.com>
2021-12-08 10:26:30 -0800

    make it better

--- porfolio_project/settings.py
+++ porfolio_project/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 #         os.path.join(BASE_DIR, 'static'),
 #    ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
