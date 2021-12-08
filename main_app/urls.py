176559ee0cf0c294bc9aafd7bece6cf8ddaa73ca
jstearne <jared.stearne@gmail.com>
2021-12-08 10:11:11 -0800

    Nothing else to do but try to see on deployed site.

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('/', views.Home.as_view(), name="home"),
 ]
--- porfolio_project/settings.py
+++ porfolio_project/settings.py
@@ -123,9 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #optional for cacheable files and compression
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+   ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = '.storage.WhiteNoiseStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
